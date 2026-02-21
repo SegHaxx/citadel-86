393310a9b0e6894dde2eff5cb76cdbcbe7692295
Seg <seg@haxxed.com>
2026-02-20 19:36:56 -0600

    Fix Y2K issue in extended directory listing, refactor date formatting

--- src/libtime.c
+++ src/libtime.c
@@ -11,6 +11,7 @@
  */
 
 #include "ctdl.h"
+#include "time.h"
 
 /*
  *				contents
@@ -52,31 +53,44 @@ char *Current_Time()
     return Time;
 }
 
-/*
- * formDate()
- *
- * This function forms the current date.
- */
-char *formDate()
-{
-    static char dateLine[40];
-    int  day, year, h, m;
-    char *month;
-
-    getCdate(&year, &month, &day, &h, &m);
-    sprintf(dateLine, "%d%s%02d", year, month, day);
-    return dateLine;
-}
-
 char  *monthTab[13] = {"", "Jan", "Feb", "Mar",
 			   "Apr", "May", "Jun",
 			   "Jul", "Aug", "Sep",
 			   "Oct", "Nov", "Dec" };
-/*
- * getCdate()
- *
- * This retrieves system date and returns in the parameters.
- */
+
+// This will return a human string representing that date.
+char* AbsToReadable(unsigned long lastdate){
+    struct tm* data;
+    char* m;
+    static char buffer[40];
+
+    /* 0l represents never in our scheme */
+    if(lastdate==0l) return "Never";
+
+    data=localtime((time_t*)&lastdate);
+    civTime(&data->tm_hour, &m);
+
+    sprintf(buffer, "%d%s%02d @ %d:%02d %s",
+			data->tm_year, monthTab[data->tm_mon + 1],
+			data->tm_mday, data->tm_hour, data->tm_min, m);
+    return buffer;
+}
+
+// Format a Citadel standard date string
+static void CitaDate(char* str,int y,int m,int d){
+    sprintf(str,"%d%s%02d",y,monthTab[m],d);
+}
+
+// This function forms the current date.
+char *formDate(){
+    int y,m,d,h,min,s,ms;
+    static char dateLine[10];
+    getRawDate(&y,&m,&d,&h,&min,&s,&ms);
+    CitaDate(dateLine,y,m,d);
+    return dateLine;
+}
+
+// This retrieves system date and returns in the parameters.
 void getCdate(int *year, char **month, int *day, int *hours, int *minutes)
 {
     int mon, seconds, milli;
@@ -86,3 +100,14 @@ void getCdate(int *year, char **month, int *day, int *hours, int *minutes)
     *month = monthTab[mon];
 }
 
+// This function converts a DOS-formatted date to a formatted string.
+void DosToNormal(char* DateStr,UNS_16 DosDate){
+	int month=(DosDate>>5)&0xf;
+	if(month>12||month<1)
+		strcpy(DateStr,"No Date");
+	else
+		CitaDate(DateStr,
+			((DosDate>>9)&0x7f)+1980,
+			month,
+			DosDate&0x1f);
+}
